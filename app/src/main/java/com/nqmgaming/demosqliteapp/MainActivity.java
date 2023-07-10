87b8954eb5a8e61c408b081487b903e28b55d60b
Nguyen Quang Minh <nguyenquangminh570@gmail.com>
2023-07-10 15:55:50 +0700

    xuất text ra strings.xml

--- app/src/main/java/com/nqmgaming/demosqliteapp/MainActivity.java
+++ app/src/main/java/com/nqmgaming/demosqliteapp/MainActivity.java
@@ -1,18 +1,29 @@
 package com.nqmgaming.demosqliteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.LocaleList;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnCategory, btnProduct;
-    private ImageButton btnGithub;
 
     private Handler handler;
     private Runnable runnable;
@@ -23,11 +34,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCategory = findViewById(R.id.btnCategory);
-        btnProduct = findViewById(R.id.btnProduct);
-        btnGithub = findViewById(R.id.btnGithub);
+        Button btnCategory = findViewById(R.id.btnCategory);
+        Button btnProduct = findViewById(R.id.btnProduct);
+        ImageButton btnGithub = findViewById(R.id.btnGithub);
+        SwitchCompat switchChangeLanguage = findViewById(R.id.switchChangeLanguage);
 
-        handler = new Handler();
+        Drawable vietnameseIcon = ResourcesCompat.getDrawable(getResources(), R.drawable.icon_vietnamese, null);
+        Drawable englishIcon = ResourcesCompat.getDrawable(getResources(), R.drawable.icon_english, null);
+
+        if (vietnameseIcon != null && englishIcon != null) {
+            int width = englishIcon.getIntrinsicWidth();
+            int height = englishIcon.getIntrinsicHeight();
+
+            StateListDrawable thumbDrawable = new StateListDrawable();
+            thumbDrawable.addState(new int[]{android.R.attr.state_checked}, vietnameseIcon);
+            thumbDrawable.addState(new int[]{}, englishIcon);
+            thumbDrawable.setBounds(0, 0, width, height);
+
+            switchChangeLanguage.setThumbDrawable(thumbDrawable);
+        } else {
+            // Xử lý trường hợp vietnameseIcon hoặc englishIcon == null nếu cần
+        }
+        Handler handler = new Handler(Looper.getMainLooper());
         runnable = new Runnable() {
             @Override
             public void run() {
@@ -58,6 +86,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        switchChangeLanguage.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    setLocale("vi");
+                } else {
+                    setLocale("en");
+                }
+                updateTexts(); // Thay thế hàm recreate() bằng hàm updateTexts()
+            }
+        });
     }
 
     @Override
@@ -77,5 +117,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         handler.removeCallbacks(runnable);
+    }
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+
+        config.setLocale(locale);
+        Context context = createConfigurationContext(config);
+        getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+    }
+
+    private void updateTexts() {
+        // Cập nhật văn bản cho các nút và các thành phần giao diện người dùng khác
+        Button btnCategory = findViewById(R.id.btnCategory);
+        Button btnProduct = findViewById(R.id.btnProduct);
+        btnCategory.setText(R.string.category);
+        btnProduct.setText(R.string.product);
     }
 }
